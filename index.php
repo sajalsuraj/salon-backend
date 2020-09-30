8da2bdb767c43a72b9f405ce6a7ced43815606f4
Sajal Suraj <sajal.suraj@gmail.com>
2020-09-30 19:28:11 +0530

    Edit, Delete and Summary

--- index.php
+++ index.php
@@ -196,6 +196,34 @@ $app->put('/edit/staff', function (Request $request, Response $response, array $
 
 });
 
+//Edit customer
+$app->put('/edit/customer', function (Request $request, Response $response, array $args) {
+    $body = $request->getParsedBody();
+    
+    $db = getDB();
+  
+    $insertSql = "update customer set name=:name, mobile=:mobile, email=:email, birthday=:birthday where id=:id";
+    $stmtInsert = $db->prepare($insertSql);
+    $stmtInsert->bindParam("name", $body['name'], PDO::PARAM_STR);
+    $stmtInsert->bindParam("email", $body['email'], PDO::PARAM_STR);
+    $stmtInsert->bindParam("birthday", $body['birthday'], PDO::PARAM_STR);
+    $stmtInsert->bindParam("mobile", $body['mobile'], PDO::PARAM_STR);
+    $stmtInsert->bindParam("id", $body['id'], PDO::PARAM_STR);
+    $stmtInsert->execute();
+
+    $rowCount = $stmtInsert->rowCount();
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Customer details updated successfully");
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Error occurred while updating, please try again");
+    }
+
+    echo json_encode($response);
+
+});
+
 //Add bill
 $app->post('/add/bill', function (Request $request, Response $response, array $args) {
     $body = $request->getParsedBody();
@@ -352,6 +380,87 @@ $app->get('/get/staff/{id}', function (Request $request, Response $response, arr
 
 });
 
+//Get customer by id
+$app->get('/get/customer/{id}', function (Request $request, Response $response, array $args) {
+
+    $db = getDB();
+    $sql = "select * from customer where id=".$args['id'];
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    $staffData = $stmt->fetch(PDO::FETCH_OBJ);
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Customer data", "data"=>$staffData);
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Customer doesn't exist");
+    }
+
+    echo json_encode($response);
+
+});
+
+//delete customer by id
+$app->delete('/delete/customer/{id}', function (Request $request, Response $response, array $args) {
+
+    $db = getDB();
+    $sql = "delete from customer where id=".$args['id'];
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Customer deleted successfully");
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Customer doesn't exist");
+    }
+
+    echo json_encode($response);
+
+});
+
+//delete staff by id
+$app->delete('/delete/staff/{id}', function (Request $request, Response $response, array $args) {
+
+    $db = getDB();
+    $sql = "delete from staff where id=".$args['id'];
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Staff deleted successfully");
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Staff doesn't exist");
+    }
+
+    echo json_encode($response);
+
+});
+
+//delete service by id
+$app->delete('/delete/service/{id}', function (Request $request, Response $response, array $args) {
+
+    $db = getDB();
+    $sql = "delete from services where id=".$args['id'];
+    $stmt = $db->prepare($sql);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Service deleted successfully");
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Service doesn't exist");
+    }
+
+    echo json_encode($response);
+
+});
+
 $app->get('/get/billings', function (Request $request, Response $response, array $args) {
 
     $db = getDB(); 
@@ -436,6 +545,33 @@ $app->post('/search/service', function (Request $request, Response $response, ar
     }
     else{
         $response = array("status"=>false, "message"=>"Service doesn't exist");
+    }
+
+    echo json_encode($response);
+
+});
+
+$app->get('/get/summary', function (Request $request, Response $response, array $args) {
+
+    $db = getDB(); 
+    $sql = "select year(created_at) as current_year,month(created_at) as current_month,sum(totalamount) as total
+    from billing where year(created_at)=:year and month(created_at)=:month
+    group by year(created_at),month(created_at)
+    order by year(created_at),month(created_at)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("year", date("Y"), PDO::PARAM_STR);
+    $stmt->bindParam("month", date("m"), PDO::PARAM_STR);
+    $stmt->execute();
+    $rowCount = $stmt->rowCount();
+    $summary = $stmt->fetch(PDO::FETCH_OBJ);
+
+    $summary->current_month = date('F', mktime(0, 0, 0, $summary->current_month, 10));
+
+    if($rowCount > 0){
+        $response = array("status"=>true, "message"=>"Summary", "data"=>$summary);
+    }
+    else{
+        $response = array("status"=>false, "message"=>"Summary doesn't exist");
     }
 
     echo json_encode($response);
